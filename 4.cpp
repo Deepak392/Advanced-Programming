8e28a9b78e0dadfd11d0af0614fa7304503abd75
Deepak Kumar Patro <69042721+Deepak392@users.noreply.github.com>
2026-08-18 10:03:40 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,35 +1,45 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-class Complex{
-    private:
-        float x;
-        float y;
-    public:
-        static int total_objects ;
-        Complex(float real, float imag){
-            this->x = real;
-            this->y = imag;
-            this->total_objects+=1;
-        }
-        void add_complex(complex a1){
-          cout<<"x: "<<this->x + a1.x<<"y: "<<"this->y +a2.y <<endl:
-        }
-        void get_details(void){
-            cout << "Real: "<<this->x<<endl;
-            cout << "Imaginary: "<<this->y<<endl;
-            cout << "Total Objects: "<<total_objects <<endl;
-        }
-        friend void sum_complex (Complex a1,Complex a2)
+
+class Complex {
+private:
+    float x;
+    float y;
+
+public:
+    static int total_objects;
+
+    Complex(float real, float imag) {
+        this->x = real;
+        this->y = imag;
+        total_objects++;
+    }
+
+    void get_details(void) {
+        cout << "Real: " << this->x << endl;
+        cout << "Imaginary: " << this->y << endl;
+        cout << "Total Objects: " << total_objects << endl;
+    }
+
+    friend void sum_complex(Complex a1, Complex a2);
 };
-void sum_complex(Complex a1, Complex a2){
-    cout << "The sum of both complex numbers is: "<<a1.x + a2.x <<"+(i"<<a1.y+a2.y<<")"<<endl;
+
+void sum_complex(Complex a1, Complex a2) {
+    cout << "The sum of both complex numbers is: "
+         << a1.x + a2.x << " + (i"
+         << a1.y + a2.y << ")" << endl;
 }
-int Complex :: total_objects = 0;
-int main(){
-    Complex c1(1.65,2.3);
+
+int Complex::total_objects = 0;
+
+int main() {
+    Complex c1(1.65, 2.3);
     c1.get_details();
-    Complex c2(1.3,5.0);
+
+    Complex c2(1.3, 5.0);
     c2.get_details();
-    c1.add_complex(c2);
+
+    sum_complex(c1, c2);
+
+    return 0;
 }
